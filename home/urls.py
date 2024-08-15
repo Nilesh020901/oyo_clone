98609e83378ee0c4facdd533e47aab5c0f409480
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-15 19:21:36 +0530

    Work on first lec and second do changes html and models.py

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name="index")
+    path('login/', views.login_page, name="login_page")
+    path('register/', views.register_page, name="register_page")
 ]
