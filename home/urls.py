74c05686286782a42646321df68481e7b41e7dcd
Nilesh020910 <nileshsoni0209@gmail.com>
2024-08-21 10:06:20 +0530

    Correct the from error which done in office

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name="index")
-    path('login/', views.login_page, name="login_page")
-    path('register/', views.register_page, name="register_page")
+    path('', views.index, name="index"),
+    
 ]
