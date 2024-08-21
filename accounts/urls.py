83ffe58da0c4bd52e9e54dfe967f8d4d5b3450df
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-21 14:21:50 +0530

    Creating Authentication with Email Verification is done in office

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
-
 from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('login/', views.login_page, name="login_page"),
-    path('register/', views.register, name="register_page"),
+    path('login/' , views.login_page, name='login_page'),
+    path('register/' , views.register, name='register'),
+
+
+    path('verify-account/<token>/', views.verify_email_token, name="verify_email_token")
+
 ]
---
+++ accounts/utils.py
@@ -0,0 +1,5 @@
+import uuid
+
+
+def generateRandomToken():
+    return str(uuid.uuid4())
