52580b9068d6e928cfbd917f1ac47559165e59e6
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-23 16:34:18 +0530

    Done lec 4 of code only need to apply command pip instal otp

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,9 @@ from accounts import views
 urlpatterns = [
     path('login/' , views.login_page, name='login_page'),
     path('register/' , views.register, name='register'),
+    path('send_otp/<email>' , views.send_otp, name='send_otp'),
+    path('verify_otp/<email>' , views.verify_otp, name='verify_otp'),
+
     path('verify-account/<token>/', views.verify_email_token, name="verify_email_token")
 
 ]
