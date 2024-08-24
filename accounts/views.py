3853a0f1bce52c153754149fa993dd98bea6e84a
Nilesh020910 <nileshsoni0209@gmail.com>
2024-08-24 23:19:28 +0530

    try

--- accounts/views.py
+++ accounts/views.py
@@ -105,7 +105,7 @@ def verify_otp(request, email):
         otp = request.POST.get('otp')
         hotel_user = HotelUser.objects.get(email = email)
 
-        if otp = hotel_user.otp:
+        if otp == hotel_user.otp:
             messages.success(request, "Login Success")
             login(request, hotel_user)
             return redirect('/account/login/')
@@ -115,34 +115,37 @@ def verify_otp(request, email):
 
     return render(request, 'verify_otp.html')
 
-    def login_vendor(request):
-        if request.method == "POST":
-            email = request.POST.get('email')
-            password = request.POST.get('password')
+def login_vendor(request):    
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-            hotel_user = HotelVendor.objects.filter(email = email)
+        hotel_user = HotelVendor.objects.filter(
+            email = email)
 
-            if not hotel_user.exists():
-                messages.warning(request, "No Account Found.")
-                return redirect('/account/login-vendor/')
 
-            if not hotel_user[0].is_verified:
-                messages.warning(request, "Account not verified")
-                return redirect('/account/login-vendor/')
-            
-            hotel_user = authenticate(username = hotel_user[0].username, password = password)
-
-            if hotel_user:
-                messages.success(request, "Login Success")
-                login(request, hotel_user)
-                return redirect('/account/dashboard/')
-
-            messages.warning(request, "Invalid credentials")
+        if not hotel_user.exists():
+            messages.warning(request, "No Account Found.")
             return redirect('/account/login-vendor/')
-        return render(request, 'vendor/login_vendor.html')
+
+        if not hotel_user[0].is_verified:
+            messages.warning(request, "Account not verified")
+            return redirect('/account/login-vendor/')
+
+        hotel_user = authenticate(username = hotel_user[0].username , password=password)
+
+        if hotel_user:
+            messages.success(request, "Login Success")
+            login(request , hotel_user)
+            return redirect('/account/dashboard/')
+
+        messages.warning(request, "Invalid credentials")
+        return redirect('/account/login-vendor/')
+    return render(request, 'vendor/login_vendor.html')
 
 def register_vendor(request):
     if request.method == "POST":
+
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         business_name = request.POST.get('business_name')
@@ -150,6 +153,14 @@ def register_vendor(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         phone_number = request.POST.get('phone_number')
+
+        hotel_user = HotelUser.objects.filter(
+            Q(email = email) | Q(phone_number  = phone_number)
+        )
+
+        if hotel_user.exists():
+            messages.warning(request, "Account exists with Email or Phone Number.")
+            return redirect('/account/register-vendor/')
 
         hotel_user = HotelVendor.objects.create(
             username = phone_number,
@@ -163,12 +174,13 @@ def register_vendor(request):
         hotel_user.set_password(password)
         hotel_user.save()
 
-        sendEmailToken(email, hotel_user.email_token)
+        sendEmailToken(email , hotel_user.email_token)
 
-        messages.success(request, "An email sent to your Emai")
+        messages.success(request, "An email Sent to your Email")
         return redirect('/account/register-vendor/')
-    return render(request, 'vendor/register_vendor.html')
 
+
+    return render(request, 'vendor/register_vendor.html')
 
 @login_required(login_url = 'login_vendor')
 def dashboard(request):
